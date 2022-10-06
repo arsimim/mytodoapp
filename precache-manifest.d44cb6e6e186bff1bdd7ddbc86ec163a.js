9ce36cc885a43a89bba4c20a8e3cb278453ae770
arsimim <arsimmatoshi@gmail.com>
2022-10-06 22:10:32 +0200

    Updates

---
+++ precache-manifest.d44cb6e6e186bff1bdd7ddbc86ec163a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "100e6ed373f7a29cf776",
+    "url": "/mytodoapp/static/css/main.68a84d05.chunk.css"
+  },
+  {
+    "revision": "100e6ed373f7a29cf776",
+    "url": "/mytodoapp/static/js/main.100e6ed3.chunk.js"
+  },
+  {
+    "revision": "9ffe73d5d50cfd1358f3",
+    "url": "/mytodoapp/static/js/1.9ffe73d5.chunk.js"
+  },
+  {
+    "revision": "5408f450da5143b89369",
+    "url": "/mytodoapp/static/js/runtime~main.5408f450.js"
+  },
+  {
+    "revision": "f7910dd9688e88bbee596a00d50b346d",
+    "url": "/mytodoapp/index.html"
+  }
+];
